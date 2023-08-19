aafa97e171d983ce469b612908b5184dae64a893
vtanh1905 <vtanh1905@gmail.com>
2023-08-19 10:13:07 +0700

    Complete terraform for the exercise

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Architecture
 ![image](https://github.com/vtanh1905/devops-practise/assets/49771724/c46b47a7-c30e-45f6-859e-6faa35ff3887)
 
+## References
+https://blog.devgenius.io/how-to-deploy-argocd-in-eks-cluster-for-continuous-deployment-6ebbb3009024
--- terraform/set_up_jenkin.sh
+++ terraform/set_up_jenkin.sh
@@ -41,3 +41,15 @@ sudo mv kubectl /usr/local/bin
 # wget https://get.helm.sh/helm-v3.12.3-linux-amd64.tar.gz
 # tar xvf helm-v3.12.3-linux-amd64.tar.gz
 # sudo mv linux-amd64/helm /usr/local/bin
+
+# Install ArgoCD
+# kubectl create namespace argocd
+# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+# Forward Port ArgoCD
+# kubectl port-forward svc/argocd-server 8080:443 -n argocd
+
+# Install ArgoCD CLI
+# curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+# sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+# rm argocd-linux-amd64
